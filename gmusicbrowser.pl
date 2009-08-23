fbd3ae6264eff0ecf10ac0556a04dc04de78f1dd
Quentin Sculo <squentin@free.fr>
2009-08-23 15:25:49 +0200

    Really fix importing filters from 1.0.x

--- gmusicbrowser.pl
+++ gmusicbrowser.pl
@@ -1476,7 +1476,7 @@ sub ReadOldSavedTags
 		my ($key,$val)=split "\x1D",$_,2;
 		$key=~s/^(.)//;
 		if ($1 eq 'F')
-		{	$val=~s/(?:^|\x1D)-?\K(\d+)?(\D)/(defined $1? Songs::FieldUpgrade($1) : '').":$1:"/ge;
+		{	$val=~s/(?:^|\x1D)-?\K(\d+)?(\D)/(defined $1? Songs::FieldUpgrade($1) : '').":$2:"/ge;
 			$val=~s/(?:^|\x1D)-?(?:label|genre)\K:e:(?=\x1D|$)/:ecount:0/g;
 			$Options{SavedFilters}{$key}=Filter->new($val);
 		}
