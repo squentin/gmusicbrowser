a60b3770bf5f349a1299156607cec157e6e40ad1
Quentin Sculo <squentin@free.fr>
2010-01-25 13:46:12 +0100

    another small fix for commit :"fixes to creation of filename/path from format"

--- gmusicbrowser.pl
+++ gmusicbrowser.pl
@@ -3993,7 +3993,7 @@ sub filenamefromformat
 }
 sub pathfromformat
 {	my ($ID,$format,$basefolder,$icase)=@_;
-	my $path= defined $basefolder.SLASH ? $basefolder : '';
+	my $path= defined $basefolder ? $basefolder.SLASH : '';
 	$path.=$1 if $format=~s#^([^\$%]*$QSLASH)##;		# move constant part of format in path
 	$path= Songs::Get($ID,'path').SLASH.$path if $path!~m#^~?$QSLASH#o; # use song's path as base for relative paths
 	$path=~s#^~($QSLASH)#$ENV{HOME}$1#o;				# replace leading ~/ by homedir
