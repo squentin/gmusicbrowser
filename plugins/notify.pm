2fa2082d979c6ac571f054966efbd408cf98b04c
Quentin Sculo <squentin@free.fr>
2010-01-28 13:53:37 +0100

    notify plugin: check daemon capabilities and small changes to preferences gui

--- plugins/notify.pm
+++ plugins/notify.pm
@@ -23,11 +23,17 @@ use Gtk2::Notify -init, ::PROGRAM_NAME;
 ::SetDefaultOptions(OPT, title => "%t", text => _"<i>by</i> %a\\n<i>from</i> %l", picsize => 50, timeout=>5);
 
 my $notify;
+my ($Daemon_name,$can_actions,$can_body);
 
 sub Start
 {	$notify=Gtk2::Notify->new('empty','empty');
 	#$notify->set_urgency('low');
 	#$notify->set_category('music'); #is there a standard category for that ?
+	my ($name, $vendor, $version, $spec_version)= Gtk2::Notify->get_server_info;
+	$Daemon_name= "$name $version ($vendor)";
+	my @caps = Gtk2::Notify->get_server_caps;
+	$can_body=	grep $_ eq 'body',	@caps;
+	$can_actions=	grep $_ eq 'actions',	@caps;
 	set_actions();
 	::Watch($notify,'PlayingSong',\&Changed);
 }
@@ -41,13 +47,17 @@ sub prefbox
 	my $sg1=Gtk2::SizeGroup->new('horizontal');
 	my $sg2=Gtk2::SizeGroup->new('horizontal');
 	my $replacetext=::MakeReplaceText('talydngLf');
-	my $entry1=::NewPrefEntry(OPT.'title',_"Title :", tip => $replacetext, sg1=> $sg1, sg2=>$sg2);
-	my $entry2=::NewPrefEntry(OPT.'text',_"Text :", tip => $replacetext."\n\n"._("You can use some markup, eg : <b>bold</b>\nNote that the markup may be ignored by the notification daemon"), sg1=> $sg1, sg2=>$sg2);
-	my $size=   ::NewPrefSpinButton(OPT.'picsize', 0,1000, step=>10, page=>40, text1=>_"Picture size :", sizeg1=>$sg1);
+	my $summary=::NewPrefEntry(OPT.'title',_"Summary :", sizeg1=> $sg1, sizeg2=>$sg2, tip => $replacetext);
+	my $body=   ::NewPrefEntry(OPT.'text', _"Body :",    sizeg1=> $sg1, sizeg2=>$sg2, width=>40, tip => $replacetext."\n\n"._("You can use some markup, eg :\n<b>bold</b> <i>italic</i> <u>underline</u>\nNote that the markup may be ignored by the notification daemon"),);
+	my $size=   ::NewPrefSpinButton(OPT.'picsize', 0,1000, step=>10, page=>40, text1=>_"Picture size :", sizeg1=>$sg1, tip=> _"Note that some notification daemons resize the displayed picture");
 	my $timeout=::NewPrefSpinButton(OPT.'timeout', 0,9999, step=>2,  page=>5,  text1=>_"Timeout :",      sizeg1=>$sg1, text2=>_"seconds", digits=>1);
 	my $actions=::NewPrefCheckButton(OPT.'actions',_"Display stop/next actions",\&set_actions);
+	$actions->set_sensitive($can_actions);
+	$actions->set_tooltip_text(_("Actions are not supported by current notification daemon").' : '.$Daemon_name) unless $can_actions;
+	$body->set_sensitive($can_body);
+	$body->set_tooltip_text(_("Body text is not supported by current notification daemon").' : '.$Daemon_name) unless $can_body;
 	my $whenhidden=::NewPrefCheckButton(OPT.'onlywhenhidden',_"Don't notify if the main window is visible");
-	$vbox->pack_start($_,::FALSE,::FALSE,2) for $entry1,$entry2,$size,$timeout,$actions,$whenhidden;
+	$vbox->pack_start($_,::FALSE,::FALSE,2) for $summary,$body,$size,$timeout,$actions,$whenhidden;
 	return $vbox;
 }
 
@@ -73,7 +83,8 @@ sub Changed
 }
 
 sub set_actions
-{	$notify->clear_actions;
+{	return unless $can_actions;
+	$notify->clear_actions;
 	if ($::Options{OPT.'actions'})
 	{	$notify->add_action('media-stop',_"Stop",\&::Stop);
 		$notify->add_action('media-next',_"Next",\&::NextSong);
