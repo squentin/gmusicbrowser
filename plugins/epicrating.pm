a41b08f35dc3b9473d6ebe2df8d579ca3e0c5302
Andrew Clunis <andrew@orospakr.ca>
2010-03-20 21:29:45 -0400

    EpicRating should have its deprecation threshold of 15 on time, not rating.  Begin adding some settings.

--- plugins/epicrating.pm
+++ plugins/epicrating.pm
@@ -42,15 +42,18 @@ sub AddRatingPointsToSong {
 sub Played {
         my $ID=$::PlayingID;
 
-        return unless defined $::PlayTime;
+        if(!defined $::PlayTime) {
+            warn "EpicRating's Played callback is missing PlayTime?!";
+            return;
+        }
         my $PlayedPartial=$::PlayTime-$::StartedAt < $::Options{PlayedPercent} * ::Songs::Get($ID,'length');
-        if ($PlayedPartial) #FIXME maybe only count as a skip if played less than ~20% ?
+        if ($PlayedPartial)
         {
                 my $song_rating = Songs::Get($ID, 'rating');
                 if(!$song_rating) {
                     ::Songs::Set($ID, rating=>50);
                 } else {
-                    if($song_rating < 15) {
+                    if($::PlayTime < 15) {
                         AddRatingPointsToSong($ID, -1);
                     } else {
                         AddRatingPointsToSong($ID, -5);
@@ -75,6 +78,15 @@ sub Stop {
 }
 
 sub prefbox {
-    my $vbox=Gtk2::VBox->new(::FALSE, 2);
-    return $vbox;
+    my $big_vbox= Gtk2::VBox->new(::FALSE, 2);
+    # rating change on skip
+    # rating change on full play
+    # if less than 15% in there somehow??!
+
+    my $rating_on_skip_box = Gtk2::HBox->new(::FALSE, 2);
+    my $rating_on_skip_entry = ::NewPrefEntry(OPT.'RATING_ON_SKIP', _"Add to rating on skip:", cb =>\&rating_on_skip_pref_changed);
+    $rating_on_skip_box->add($rating_on_skip_entry);
+    $big_vbox->add($rating_on_skip_box);
+    $big_vbox->show_all();
+    return $big_vbox;
 }
