7915bcccedfa01b74b94c0a9432360e6da5ce0db
Andrew Clunis <andrew@orospakr.ca>
2010-04-11 22:24:34 -0400

    [DACP Remote] now doing some path routing and query reading in the HTTP server.

--- plugins/dacp_remote.pm
+++ plugins/dacp_remote.pm
@@ -31,8 +31,13 @@ package GMB::Plugin::DACPREMOTE;
 use strict;
 use warnings;
 
+# actually get *feedback* instead of silence if a session crashes.
+sub POE::Kernel::CATCH_EXCEPTIONS () { 0 }
+
 use POE::Kernel { loop => "Glib" };
 use POE::Component::Server::HTTP;
+
+use CGI;
 use HTTP::Status qw/RC_OK/;
 
 use constant {
@@ -41,32 +46,80 @@ use constant {
 
 my $self=bless {}, __PACKAGE__;
 
+sub cgi_from_request {
+    my ($request) = @_;
+    
+    # I'm not really sure why POE/HTTP isn't
+    # doing this for me...
+    # this implementation is rather naiive.
+    my $q;
+    if ($request->method() eq 'POST') {
+	$q = new CGI($request->content);
+    }
+    else {
+	$request->uri() =~ /\?(.+$)/;
+	if (defined($1)) {
+	    $q = new CGI($1);
+	}
+	else {
+	    $q = new CGI;
+	}
+    }
+    return $q;
+}
 
-sub web_handler {
-  my ($request, $response) = @_;
+sub login_handler {
+    my ($request, $response) = @_;
+    warn "Login request received: " . $request;
 
-  my $ID = $::PlayingID;
+    my $cgi = cgi_from_request($request);
 
-  my $playing_song;
-  if(defined $ID) {
-      $playing_song = "Playing song: " . ::Songs::Get($ID, 'artist') . " - " . ::Songs::Get($ID, 'title');
-  } else {
-      $playing_song = "None! :(";
-  }
+    warn "got cgi";
+    my $pairing_guid = $cgi->param("pairing-guid");
+    warn "... got pairing-guid: " . $pairing_guid;
 
-  # Build the response.
-  $response->code(RC_OK);
-  $response->push_header("Content-Type", "text/plain");
-  $response->content($playing_song);
+    # Build the response.
+    $response->code(RC_OK);
+    $response->push_header("Content-Type", "text/plain");
+    $response->content("");
 
-  # Signal that the request was handled okay.
-  return RC_OK;
+    # Signal that the request was handled okay.
+    return RC_OK;
+}
+
+sub root_handler {
+    my ($request, $response) = @_;
+
+    my $path = $request->uri->path;
+    my $query = $request->uri->query;
+
+    warn "HTTP request to root or unknown: " . $request->uri->path;
+
+    my $ID = $::PlayingID;
+
+
+
+    my $playing_song;
+    if(defined $ID) {
+	$playing_song = "Playing song: " . ::Songs::Get($ID, 'artist') . " - " . ::Songs::Get($ID, 'title');
+    } else {
+	$playing_song = "None! :(";
+    }
+    
+    # Build the response.
+    $response->code(RC_OK);
+    $response->push_header("Content-Type", "text/plain");
+    $response->content($playing_song);
+
+    # Signal that the request was handled okay.
+    return RC_OK;
 }
 
 sub Start {
     POE::Component::Server::HTTP->new(
-	Port           => 8080,
-	ContentHandler => {"/" => \&web_handler},
+	Port           => 3689,
+	ContentHandler => {"/" => \&root_handler,
+	                   "/login" => \&login_handler},
 	Headers => {Server => 'Gmusicbrowser DACP',},
     );
 
