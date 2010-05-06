551731cf7befa66b2ca121d51989c69cf8b631c2
Andrew Clunis <andrew@orospakr.ca>
2010-05-05 22:40:45 -0400

    [HTTP Server] F'up implementation of skip.  Something is erroring, silently.  Where is the exception?

--- plugins/http_server.pm
+++ plugins/http_server.pm
@@ -10,7 +10,7 @@
 # in the Ubuntu (Debian?) packages.
 # As such, it needs to be loaded from CPAN (plus dependencies)
 
-# cpan -i POE POE::Loop::Glib POE::Component::Server::HTTP Net::DAAP::DMAP Net::Rendezvous::Publish::Backend::Avahi
+# cpan -i POE POE::Loop::Glib POE::Component::Server::HTTP CGI::Application::Dispatch
 
 =gmbplugin HTTPSERVER
 name    HTTP Server
@@ -31,7 +31,7 @@ use CGI;
 use HTTP::Status qw/RC_OK/;
 
 # actually get *feedback* instead of silence if a session crashes.
-# sub POE::Kernel::CATCH_EXCEPTIONS () { 0 }
+sub POE::Kernel::CATCH_EXCEPTIONS () { 0 }
 
 
 # my $cgi = cgi_from_request($request);
@@ -64,7 +64,22 @@ sub player_handler {
     my $path = $request->uri->path;
     my $query = $request->uri->query;
 
-    $cgi = cgi_from_request($request);
+    my $cgi = cgi_from_request($request);
+}
+
+sub skip_handler {
+    my ($request, $response) = @_;
+    my $path = $request->uri->path;
+    my $query = $request->uri->query;
+
+    warn "Got Skip request!";
+    my $cgi = cgi_from_request($request);
+
+    NextSong();
+    warn "Successfully skipped song!";
+
+    $response->code(RC_OK);
+    return RC_OK;
 }
 
 sub root_handler {
@@ -83,6 +98,7 @@ sub root_handler {
 	$playing_song = "None! :(";
     }
     
+    $playing_song .= "<br /><br /><a href=\"/skip\">Skip!</a>";
     # Build the response.
     $response->code(RC_OK);
 #    $response->push_header("Content-Type", "application/x-dmap-tagged");
@@ -96,7 +112,8 @@ sub Start {
     my $http = POE::Component::Server::HTTP->new(
     	Port           => 8080,
     	ContentHandler => {"/" => \&root_handler,
-			   "/player" => \&player_handler
+			   "/player" => \&player_handler,
+			   "/skip" => \&skip_handler
     	},
     	Headers => {Server => 'Gmusicbrowser HTTP',},
     );
