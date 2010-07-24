eb865323d8977730237ce1b5e7093614dd335e86
Andrew Clunis <andrew@orospakr.ca>
2010-07-24 10:50:15 -0400

    [HTTP Server] Encode some basic player state into JSON, have Javascript code update simple web UI with it whenever user actions happen.

--- plugins/http_server.pm
+++ plugins/http_server.pm
@@ -23,6 +23,8 @@ package GMB::Plugin::HTTPSERVER;
 use strict;
 use warnings;
 
+use JSON;
+
 use POE;
 use POE::Kernel { loop => "Glib" };
 use POE::Component::Server::HTTP;
@@ -41,6 +43,15 @@ sub POE::Kernel::CATCH_EXCEPTIONS () { 0 }
 
 my $resource_path;
 my $resources = {};
+
+sub song2json {
+    my ($song_id) = @_;
+    return {"id" => $song_id, "artist" => ::Songs::Get($song_id, "artist"), "title" => ::Songs::Get($song_id, "title")};
+}
+
+sub state2json {
+    return {"current" => song2json($::SongID), "playing" => $::TogPlay};
+}
 
 sub cgi_from_request {
     my ($request) = @_;
@@ -76,8 +87,9 @@ sub skip_handler {
     ::NextSong();
     warn "Successfully skipped song!";
 
-    $response->code(302);
-    $response->header('Location' => "/");
+    $response->code(200);
+    $response->content_type('application/json');
+    $response->content(encode_json(state2json()));
     return RC_OK;
 }
 
@@ -98,6 +110,19 @@ sub volume_handler {
     return RC_OK;
 }
 
+sub player_handler {
+    my ($request, $response) = @_;
+    $response->protocol( "HTTP/1.1" );
+    $request->header(Connection => 'close');
+    my $path = $request->uri->path;
+    my $query = $request->uri->query;
+
+    $response->code(200);
+    $response->content_type('application/json');
+    $response->content(encode_json(state2json()));
+    return RC_OK;
+}
+
 sub playpause_handler {
     my ($request, $response) = @_;
     $response->protocol( "HTTP/1.1" );
@@ -111,7 +136,8 @@ sub playpause_handler {
     ::PlayPause();
 
     $response->code(200);
-    $response->content("sadfsafaf");
+    $response->content_type('application/json');
+    $response->content(encode_json(state2json()));
     return RC_OK;
 }
 
@@ -122,8 +148,6 @@ sub code_handler {
     my $query = $request->uri->query;
 
     my @split_request = split(/\//, $path);
-
-
 
     my $local_filename = $resource_path.::SLASH.'http_server'.::SLASH.$split_request[2];
 
@@ -180,6 +204,12 @@ sub root_handler {
     </style>
   </head>
   <body>
+    <div id=current_song>
+      <span id=current_song_artist></span> - 
+      <span id=current_song_title></span>
+    </div>
+    <br>
+
     <div id=seek_slider>
       <div id=seek_position></div>
     </div>
@@ -210,6 +240,7 @@ sub Start {
     my $http = POE::Component::Server::HTTP->new(
     	Port           => 8080,
     	ContentHandler => {"/" => \&root_handler,
+			   "/player" => \&player_handler,
 			   "/noscript" => \&root_noscript_handler,
 			   "/skip" => \&skip_handler,
 			   "/playpause" => \&playpause_handler,
--- plugins/http_server/player.js
+++ plugins/http_server/player.js
@@ -1,23 +1,67 @@
 var GmusicBrowserClient = Class.create({
     initialize: function() {
-	
+	this.update_callbacks = new Array();
+    },
+
+    onUpdate: function(cb) {
+	this.update_callbacks.push(cb);
+    },
+
+    update: function(json_status) {
+	this.update_callbacks.each(function(cb) {
+	    this.state_description = json_status.evalJSON();
+	    cb(this.state_description);
+	}.bind(this));
+    },
+
+    getStateUpdate: function() {
+	// is there a better way to do this? scoping isn't quite what I expect evidently.
+	var thiz = this;
+	new Ajax.Request("/player", {
+	    method: 'get',
+	    onSuccess: function(response) {
+		thiz.update(response.responseText);
+	    },
+	    onFailure: function(response) {
+		alert("holy crap problem play/pause'ing");
+	    }
+	});
     },
 
     playpause: function() {
-	new Ajax.Request("/playpause");
+	// is there a better way to do this? scoping isn't quite what I expect evidently.
+	var thiz = this;
+	new Ajax.Request("/playpause", {
+	    method: 'post',
+	    onSuccess: function(response) {
+		thiz.update(response.responseText);
+	    },
+	    onFailure: function(response) {
+		alert("holy crap problem play/pause'ing");
+	    }
+	});
     },
 
     skip: function() {
-	new Ajax.Request("/skip");
+	var thiz = this;
+	new Ajax.Request("/skip", {
+	    method: 'post',
+	    onSuccess: function(response) {
+		thiz.update(response.responseText);
+	    },
+	    onFailure: function(response) {
+		alert("holy crap problem skipping");
+	    }
+	});
     },
 
     setVolume: function(volume) {
 	new Ajax.Request("/volume", {
 	    method: "post",
-	    onSuccess: function(transport) {
+	    onSuccess: function(response) {
 		// TODO check return value, factor out said checking
 	    }.bind(this),
-	    onFailure: function(transport) {
+	    onFailure: function(response) {
 		alert("holy shit problem setting volume");
 	    }.bind(this),
 	    parameters: {volume:volume}
@@ -26,6 +70,19 @@ var GmusicBrowserClient = Class.create({
 });
 
 var gmb = new GmusicBrowserClient();
+
+var do_update = function(state_description) {
+    // TODO we may not get all fields here, only update ones that actually are.
+    $('current_song_title').innerHTML = state_description.current.title.escapeHTML();
+    $('current_song_artist').innerHTML = state_description.current.artist.escapeHTML();
+    if(state_description.playing == 1) {
+	$('playpausebutton').innerHTML = "Pause";
+    } else {
+	$('playpausebutton').innerHTML = "Play";
+    }
+}.bind(this);
+
+gmb.onUpdate(do_update);
 
 var volume = new Control.Slider('seek_position', 'seek_slider', {
   axis:'horizontal',
@@ -36,17 +93,15 @@ var volume = new Control.Slider('seek_position', 'seek_slider', {
 });
 
 volume.options.onSlide = function(value) {
-    console.log("whooooo: " + value);
     gmb.setVolume(value);
 };
 
 Event.observe('playpausebutton', 'click', function(event) {
-    // TODO should be validating 200 OK
     gmb.playpause();
 });
 
 Event.observe('skipbutton', 'click', function(event) { 
-    // TODO should be validating 200 OK
     gmb.skip();
 });
 
+gmb.getStateUpdate();
