squentin/gmusicbrowser

commit ad6a325c9eff68767cfa333711bf3693996520dc

add format options for album/artist FilterLists

gmusicbrowser.pl
@@ -499,7 +499,6 @@ sub Gtk2::Label::set_markup_with_format
 {	my $label=shift;
 	$label->set_markup( MarkupFormat(@_) );
 }
-
 sub IncSuffix	# increment a number suffix from a string
 {	$_[0] =~ s/(?<=\D)(\d*)$/($1||1)+1/e;
 }
@@ -3503,6 +3502,7 @@ sub BuildChoiceMenu
 			$item->{selected}= $value;
 			$item->signal_connect(activate => $smenu_callback, $options{code} );
 		}
+		$item->child->set_markup( $item->child->get_label ) if $options{submenu_ordered_hash};
 		$menu->append($item);
 	}
 	$menu=undef unless @order; #empty submenu

gmusicbrowser_list.pm
@@ -1500,6 +1500,13 @@ our %Pages=
 	filesys	=> [Filesystem		=> '',			'',_"Filesystem"],
 );
 
+our @MenuMarkupOptions=
+(	"%a",
+	"<b>%a</b>%Y\n<small>%s <small>%l</small></small>",
+	"<b>%a</b>%Y\n<small>%b</small>",
+	"<b>%a</b>%Y\n<small>%b</small>\n<small>%s <small>%l</small></small>",
+	"<b>%y %a</b>",
+);
 my @picsize_menu=
 (	_("no pictures")	=>  0,
 	_("automatic size")	=> -1,
@@ -1541,7 +1548,11 @@ my @MenuSubGroup=
 (	{ label => _"show pictures",	code => sub { my $self=$_[0]{self}; $self->{lpicsize}[$_[0]{depth}]=$_[1]; $self->SetOption; },	mode => 'LS',
 	  submenu => \@picsize_menu,	submenu_ordered_hash => 1,  check => sub {$_[0]{self}{lpicsize}[$_[0]{depth}]},
 		test => sub { Songs::FilterListProp($_[0]{subfield},'picture'); }, },
-	{ label => _"show info",	code => sub { my $self=$_[0]{self}; $self->{lmarkup}[$_[0]{depth}]^=1; $self->SetOption; },
+	{ label => _"text format",	code => sub { my $self=$_[0]{self}; $self->{lmarkup}[$_[0]{depth}]= $_[1]; $self->SetOption; },
+	  submenu => sub{	my $field= $_[0]{self}{field}[ $_[0]{depth} ];
+		  		my $gid=$_[0]{gidlist}[0]; return unless $gid; #FIXME option not shown when no gid is displayed, find a better way to display example format
+		  		return [ map { AA::ReplaceFields( $gid,$_,$field,::TRUE ), ($_ eq "%a" ? 0 : $_) } @MenuMarkupOptions ];
+	  		},	submenu_ordered_hash => 1, submenu_use_markup => 1,
 	  check => sub { $_[0]{self}{lmarkup}[$_[0]{depth}]}, istrue => 'aa', mode => 'LS', },
 	{ label => _"show info",	code => sub { my $self=$_[0]{self}; $self->{mmarkup}^=1; $self->SetOption; },
 	  check => sub { $_[0]{self}{mmarkup} }, mode => 'M', },
@@ -3964,7 +3975,7 @@ sub makelayout
 	my $layout=Gtk2::Pango::Layout->new( $widget->create_pango_context );
 	my $field=$prop->[P_FIELD][$depth];
 	my $markup=$prop->[P_MARKUP][$depth];
-	$markup= $markup ? "<b>%a</b>%Y\n<small>%s <small>%l</small></small>" : "%a"; #FIXME
+	$markup= !$markup ? "%a" : $markup eq 1 ? "<b>%a</b>%Y\n<small>%s <small>%l</small></small>" : $markup;
 	if ($gid==FilterList::GID_ALL)
 	{	$markup= ::MarkupFormat("<b>%s (%d)</b>", Songs::Field_All_string($field), $cell->get('all_count') );
 	}

gmusicbrowser_songs.pm
@@ -1896,7 +1896,12 @@ our %ReplaceFields=
 	s	=>	sub { my $l=Get('idlist',$_[0],$_[1])||[]; ::__('%d song','%d songs',scalar @$l) },
 	x	=>	sub { my $nb=@{GetXRef($_[0],$_[1])}; return $_[0] ne 'album' ? ::__("%d Album","%d Albums",$nb) : ::__("%d Artist","%d Artists",$nb);  },
 	X	=>	sub { my $nb=@{GetXRef($_[0],$_[1])}; return $_[0] ne 'album' ? ::__("%d Album","%d Albums",$nb) : $nb>1 ? ::__("%d Artist","%d Artists",$nb) : '';  },
-	b	=>	sub { my $l=Songs::UniqList('artist', Get('idlist',$_[0],$_[1])); return @$l==1 ? Songs::Gid_to_Display('artist',$l->[0]) : ::__("%d artist","%d artists", scalar(@$l));  },
+	b	=>	sub {	if ($_[0] ne 'album') { my $nb=@{GetXRef($_[0],$_[1])}; return ::__("%d Album","%d Albums",$nb); }
+				else
+				{	my $l=Songs::UniqList('artist', Get('idlist',$_[0],$_[1]));
+					return @$l==1 ? Songs::Gid_to_Display('artist',$l->[0]) : ::__("%d artist","%d artists", scalar(@$l));
+				}
+			    },
 );
 
 sub ReplaceFields

commit dcf6317bbc9cfacd9a22ef160b8a3ec6180111b5

fix stupid bug causing markup warnings in some cases
introduced in ad6a325c9eff68767cf

gmusicbrowser.pl
@@ -3510,7 +3510,7 @@ sub BuildChoiceMenu
 			$item->{selected}= $value;
 			$item->signal_connect(activate => $smenu_callback, $options{code} );
 		}
-		$item->child->set_markup( $item->child->get_label ) if $options{submenu_ordered_hash};
+		$item->child->set_markup( $item->child->get_label ) if $options{submenu_use_markup};
 		$menu->append($item);
 	}
 	$menu=undef unless @order; #empty submenu