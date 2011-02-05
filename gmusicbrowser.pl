8d72bd2c5650db0815cc580cd78007dfc4d0d3b5
tin <girlwithoutadream@gmail.com>
2011-02-05 18:10:03 +0100

    add russian translation

--- gmusicbrowser.pl
+++ gmusicbrowser.pl
@@ -5202,6 +5202,7 @@ sub AboutDialog
 		'Czech : Vašek Kovářík',
 		'Portuguese : Gleriston Sampaio <gleriston_sampaio@hotmail.com>',
 		'Korean : bluealbum',
+		'Russian : tin',
 	);
 	$dialog->signal_connect( response => sub { $_[0]->destroy if $_[1] eq 'cancel'; }); #used to worked without this, see http://mail.gnome.org/archives/gtk-perl-list/2006-November/msg00035.html
 	$dialog->show_all;
