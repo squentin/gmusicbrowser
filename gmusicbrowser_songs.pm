c2667f619346684b6387b8e2c3edc763235fdc4a
Quentin Sculo <squentin@free.fr>
2011-03-23 19:47:43 +0100

    fix icons for labels with spaces or special characters not working

--- gmusicbrowser_songs.pm
+++ gmusicbrowser_songs.pm
@@ -610,8 +610,8 @@ our %timespan_menu=
 	 #is_set	=> '(__LABEL__=~m/(?:^|\x00)__QVAL__(?:$|\x00)/)? 1 : 0', #for random mode
 	type		=> 'flags',		init_namearray	=> '@{$::Options{Labels}}',
 	iconprefix	=> 'label-',
-	icon		=> sub { $Def{label}{iconprefix}.::url_escape($_[0]); }, #FIXME use icon_for_gid
-	icon_for_gid	=> '"#iconprefix#".::url_escape(#gid_to_get#)',
+	icon		=> sub { $Def{label}{iconprefix}.$_[0]; }, #FIXME use icon_for_gid
+	icon_for_gid	=> '"#iconprefix#".#gid_to_get#',
 	all_count	=> _"All labels",
 	none		=> quotemeta _"No label",
 	FilterList	=> {search=>1,icon=>1},
@@ -1738,7 +1738,7 @@ sub ChooseIcon	 #FIXME add a way to create a colored square/circle/... icon
 	return unless defined $file;
 	my $dir=$::HomeDir.'icons';
 	return if ::CreateDir($dir) ne 'ok';
-	my $destfile= $dir. ::SLASH. Picture($gid,$field,'icon');
+	my $destfile= $dir. ::SLASH. ::url_escape( Picture($gid,$field,'icon') );
 	unlink $destfile.'.svg',$destfile.'.png';
 	if ($file eq '0') {}	#unset icon
 	elsif ($file=~m/\.svg/i)
