b9a802d93506795641c741cac599ccb967df2650
Michele Giampaolo <neuferkar@hotmail.com>
2011-04-26 21:44:55 +0200

    add italian translation

--- gmusicbrowser.pl
+++ gmusicbrowser.pl
@@ -5231,6 +5231,7 @@ sub AboutDialog
 		'Portuguese : Gleriston Sampaio <gleriston_sampaio@hotmail.com>',
 		'Korean : bluealbum',
 		'Russian : tin',
+		'Italian : Michele Giampaolo',
 	);
 	$dialog->signal_connect( response => sub { $_[0]->destroy if $_[1] eq 'cancel'; }); #used to worked without this, see http://mail.gnome.org/archives/gtk-perl-list/2006-November/msg00035.html
 	$dialog->show_all;
