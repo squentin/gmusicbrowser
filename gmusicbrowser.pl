squentin/gmusicbrowser

commit be8b77d932c668080f2746a446435abe997a865b

close traytip imediately on exit

gmusicbrowser.pl
@@ -1425,6 +1425,7 @@ sub Quit
 	$Options{SavedPlayTime}= $PlayTime if $Options{RememberPlayTime};
 	&Stop if defined $TogPlay;
 	@ToScan=@ToAdd_Files=();
+	CloseTrayTip();
 	SaveTags();
 	unlink $FIFOFile if defined $FIFOFile;
 	Gtk2->main_quit;
@@ -6854,18 +6855,20 @@ sub SetTrayTipDelay
 	$TrayIcon->child->{hover_delay}= $Options{TrayTipDelay}||1;
 }
 sub TrayMenuPopup
-{	my $traytip=$TrayIcon->child->{PoppedUpWindow};
-	$traytip->DestroyNow if $traytip;
+{	CloseTrayTip();
 	$TrayIcon->{block_popup}=1;
 	my $menu=Gtk2::Menu->new;
 	$menu->signal_connect( selection_done => sub {$TrayIcon->{block_popup}=undef});
 	PopupContextMenu(\@TrayMenu, {usemenupos=>1}, $menu);
 }
+sub CloseTrayTip
+{	my $traytip=$TrayIcon->child->{PoppedUpWindow};
+	$traytip->DestroyNow if $traytip;
+}
 sub ShowTraytip
 {	return 0 if !$TrayIcon || $TrayIcon->{block_popup};
 	Layout::Window::Popup::Popup($TrayIcon->child,$_[0]);
 }
-
 sub windowpos	# function to position window next to clicked widget ($event can be a widget)
 {	my ($win,$event)=@_;
 	return (0,0) unless $event;

commit 2d4efd6e1bed6a5fec18e793651a20d5f8f2de71

fix error when quitting if there is not a tray icon
bug introduced in commit be8b77d932

gmusicbrowser.pl
@@ -6862,7 +6862,8 @@ sub TrayMenuPopup
 	PopupContextMenu(\@TrayMenu, {usemenupos=>1}, $menu);
 }
 sub CloseTrayTip
-{	my $traytip=$TrayIcon->child->{PoppedUpWindow};
+{	return unless $TrayIcon;
+	my $traytip=$TrayIcon->child->{PoppedUpWindow};
 	$traytip->DestroyNow if $traytip;
 }
 sub ShowTraytip