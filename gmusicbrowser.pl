9dc517a247616ab32b9f652d863d9fafbc57a20c
Jiri Grönroos <jiri.gronroos+l10n@iki.fi>
2013-02-08 18:44:44 +0100

    add partial Finnish translation

--- gmusicbrowser.pl
+++ gmusicbrowser.pl
@@ -5638,6 +5638,7 @@ sub AboutDialog
 		'Dutch : Gijs Timmers',
 		'Japanese : Sunatomo',
 		'Serbian : Саша Петровић',
+		'Finnish : Jiri Grönroos',
 	);
 	$dialog->signal_connect( response => sub { $_[0]->destroy if $_[1] eq 'cancel'; }); #used to worked without this, see http://mail.gnome.org/archives/gtk-perl-list/2006-November/msg00035.html
 	$dialog->show_all;
