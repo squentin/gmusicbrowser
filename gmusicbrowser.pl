11050ba20c6f14f1d7f7c6de7ade6d98e0287916
Hiunn-hué <hiunnhue108@ymail.com>
2013-02-11 18:22:12 +0100

    add partial Chinese(Taiwan) translation

--- gmusicbrowser.pl
+++ gmusicbrowser.pl
@@ -5639,6 +5639,7 @@ sub AboutDialog
 		'Japanese : Sunatomo',
 		'Serbian : Саша Петровић',
 		'Finnish : Jiri Grönroos',
+		'Chinese(Taiwan) : Hiunn-hué',
 	);
 	$dialog->signal_connect( response => sub { $_[0]->destroy if $_[1] eq 'cancel'; }); #used to worked without this, see http://mail.gnome.org/archives/gtk-perl-list/2006-November/msg00035.html
 	$dialog->show_all;
