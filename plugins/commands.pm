1e4625900e06ca74649bedb9387b904f3891ffca
mrlee14 <morenojee@gmail.com>
2016-01-24 18:02:30 +0530

    added 4 command fields; needs some optimizations... but seems to work

--- plugins/commands.pm
+++ plugins/commands.pm
@@ -8,7 +8,7 @@
 =for gmbplugin Commands
 name	Commands
 title	Commands plugin
-desc	Adds multiple user command fields
+desc	Multiple user commands for execution of custom commands on selected files
 =cut
 
 package GMB::Plugin::Commands;
@@ -20,45 +20,45 @@ use constant
 
 my $ON;
 my %menuentry=
-(tocmd =>
- {	label => sub { ($::Options{OPT.'tocmd_label'} || _"Unnamed custom command") },
-	code => \&RunCommand,
-	test => sub {my $c=$::Options{OPT.'tocmd_cmd'}; defined $c && $c ne '';},
+(tocmd_1 =>
+ {	label => sub { ($::Options{OPT.'tocmd_label_1'} || _"Unnamed custom command") },
+	code => \&RunCommand_1,
+	test => sub {my $c=$::Options{OPT.'tocmd_cmd_1'}; defined $c && $c ne '';},
+	notempty => 'IDs',
+ },
+tocmd_2 =>
+ {	label => sub { ($::Options{OPT.'tocmd_label_2'} || _"Unnamed custom command") },
+	code => \&RunCommand_2,
+	test => sub {my $c=$::Options{OPT.'tocmd_cmd_2'}; defined $c && $c ne '';},
+	notempty => 'IDs',
+ },
+tocmd_3 =>
+ {	label => sub { ($::Options{OPT.'tocmd_label_3'} || _"Unnamed custom command") },
+	code => \&RunCommand_3,
+	test => sub {my $c=$::Options{OPT.'tocmd_cmd_3'}; defined $c && $c ne '';},
+	notempty => 'IDs',
+ },
+ tocmd_4 =>
+ {	label => sub { ($::Options{OPT.'tocmd_label_4'} || _"Unnamed custom command") },
+	code => \&RunCommand_4,
+	test => sub {my $c=$::Options{OPT.'tocmd_cmd_4'}; defined $c && $c ne '';},
+	notempty => 'IDs',
+ },
+ tocmd_5 =>
+ {	label => sub { ($::Options{OPT.'tocmd_label_5'} || _"Unnamed custom command") },
+	code => \&RunCommand_5,
+	test => sub {my $c=$::Options{OPT.'tocmd_cmd_5'}; defined $c && $c ne '';},
 	notempty => 'IDs',
  }
-);
-
-my %FLmenuentry=
-(topath =>
- {	label => _"Copy to portable player",
-	code => \&Copy,
-	test => \&checkvalidpath,
-	isdefined => 'filter',
- },
- tom3u =>
- {	label => _"Export to .m3u file",
-	code => \&ToM3U,
-	isdefined => 'filter',
- },
-);
-
-my %exportbutton=
-(	class	=> 'Layout::Button',
-	stock	=> 'gtk-save',
-	tip	=> _"Export to .m3u file",
-	activate=> sub { my $array=::GetSongArray($_[0]); ToM3U({IDs=>$array}) if $array; },
-	#autoadd_type	=> 'button songs',
 );
 
 sub Start
 {	$ON=1;
 	updatemenu();
-	Layout::RegisterWidget(PluginM3UExport=>\%exportbutton);
 }
 sub Stop
 {	$ON=0;
 	updatemenu();
-	Layout::RegisterWidget(PluginM3UExport=>undef);
 }
 
 sub prefbox
@@ -66,22 +66,51 @@ sub prefbox
 	my $sg1=Gtk2::SizeGroup->new('horizontal');
 	my $sg2=Gtk2::SizeGroup->new('horizontal');
 
-	#my $entry1=::NewPrefFileEntry(OPT.'path',_"Player mounted on :",folder=>1, sizeg1=>$sg1,sizeg2=>$sg2);
-	#my $entry2=::NewPrefEntry(OPT.'folderformat',_"Folder format :", sizeg1=>$sg1,sizeg2=>$sg2, tip =>_("These fields can be used :")."\n".::MakeReplaceText('talydnAY'));
-	#my $entry3=::NewPrefEntry(OPT.'filenameformat',_"Filename format :", sizeg1=>$sg1,sizeg2=>$sg2, tip =>_("These fields can be used :")."\n".::MakeReplaceText('talydnAYo'));
-	#my $check1=::NewPrefCheckButton(OPT.'topath',_"Copy to mounted portable player", cb=>\&updatemenu, widget=> ::Vpack($entry1,$entry2,$entry3) );
-
-	my $entry4=::NewPrefEntry(OPT.'tocmd_label',_"Menu entry name", sizeg1=> $sg1,sizeg2=>$sg2, tip => _("Name under which the command will appear in the menu"));
-	my $entry5=::NewPrefEntry(OPT.'tocmd_cmd',_"System command :", sizeg1=> $sg1,sizeg2=>$sg2,
+	# CMD1
+	my $entry1=::NewPrefEntry(OPT.'tocmd_label_1',_"Menu entry name", sizeg1=> $sg1,tip => _("Name under which the command will appear in the menu"));
+	my $entry2=::NewPrefEntry(OPT.'tocmd_cmd_1',_"System command :", sizeg1=> $sg1,
 		tip =>  _("These fields can be used :")."\n".::MakeReplaceText('ftalydnAY')."\n".
 			_("In this case one command by file will be run\n\n").
 			_('Or you can use the field $files which will be replaced by the list of files, and only one command will be run'));
-	my $check2=::NewPrefCheckButton(OPT.'tocmd',_"Execute custom command on selected files", cb=>\&updatemenu, widget=> ::Vpack($entry4,$entry5) );
+	my $check1=::NewPrefCheckButton(OPT.'tocmd_1',_"Command 1", cb=>\&updatemenu, widget=> ::Vpack($entry1,$entry2) );
+	$vbox->pack_start($_,::FALSE,::FALSE,2) for $check1;
 
-	#my $check3=::NewPrefCheckButton(OPT.'tom3u',_"Export to .m3u file", cb=>\&updatemenu);
-	#my $check4=::NewPrefCheckButton(OPT.'toCSV',_"Export song properties to a .csv file", cb=>\&updatemenu);
-	#$vbox->pack_start($_,::FALSE,::FALSE,2) for $check1,$check2,$check3,$check4;
+	# CMD2
+	my $entry3=::NewPrefEntry(OPT.'tocmd_label_2',_"Menu entry name", sizeg1=> $sg1,tip => _("Name under which the command will appear in the menu"));
+	my $entry4=::NewPrefEntry(OPT.'tocmd_cmd_2',_"System command :", sizeg1=> $sg1,
+		tip =>  _("These fields can be used :")."\n".::MakeReplaceText('ftalydnAY')."\n".
+			_("In this case one command by file will be run\n\n").
+			_('Or you can use the field $files which will be replaced by the list of files, and only one command will be run'));
+	my $check2=::NewPrefCheckButton(OPT.'tocmd_2',_"Command 2", cb=>\&updatemenu, widget=> ::Vpack($entry3,$entry4));
 	$vbox->pack_start($_,::FALSE,::FALSE,2) for $check2;
+
+	# CMD3
+	my $entry5=::NewPrefEntry(OPT.'tocmd_label_3',_"Menu entry name", sizeg1=> $sg1,tip => _("Name under which the command will appear in the menu"));
+	my $entry6=::NewPrefEntry(OPT.'tocmd_cmd_3',_"System command :", sizeg1=> $sg1,
+		tip =>  _("These fields can be used :")."\n".::MakeReplaceText('ftalydnAY')."\n".
+			_("In this case one command by file will be run\n\n").
+			_('Or you can use the field $files which will be replaced by the list of files, and only one command will be run'));
+	my $check3=::NewPrefCheckButton(OPT.'tocmd_3',_"Command 3", cb=>\&updatemenu, widget=> ::Vpack($entry5,$entry6) );
+	$vbox->pack_start($_,::FALSE,::FALSE,2) for $check3;
+
+	# CMD4
+	my $entry6=::NewPrefEntry(OPT.'tocmd_label_4',_"Menu entry name", sizeg1=> $sg1,tip => _("Name under which the command will appear in the menu"));
+	my $entry7=::NewPrefEntry(OPT.'tocmd_cmd_4',_"System command :", sizeg1=> $sg1,
+		tip =>  _("These fields can be used :")."\n".::MakeReplaceText('ftalydnAY')."\n".
+			_("In this case one command by file will be run\n\n").
+			_('Or you can use the field $files which will be replaced by the list of files, and only one command will be run'));
+	my $check4=::NewPrefCheckButton(OPT.'tocmd_4',_"Command 4", cb=>\&updatemenu, widget=> ::Vpack($entry6,$entry7));
+	$vbox->pack_start($_,::FALSE,::FALSE,2) for $check4;
+
+#	# CMD5
+#	my $entry8=::NewPrefEntry(OPT.'tocmd_label_5',_"Menu entry name", sizeg1=> $sg1,tip => _("Name under which the command will appear in the menu"));
+#	my $entry9=::NewPrefEntry(OPT.'tocmd_cmd_5',_"System command :", sizeg1=> $sg1,
+#		tip =>  _("These fields can be used :")."\n".::MakeReplaceText('ftalydnAY')."\n".
+#			_("In this case one command by file will be run\n\n").
+#			_('Or you can use the field $files which will be replaced by the list of files, and only one command will be run'));
+#	my $check5=::NewPrefCheckButton(OPT.'tocmd_5',_"Command 5", cb=>\&updatemenu, widget=> ::Vpack($entry8,$entry9) );
+#	$vbox->pack_start($_,::FALSE,::FALSE,2) for $check5;
+
 	return $vbox;
 }
 
@@ -97,86 +126,34 @@ sub updatemenu
 		{	@$menu =grep $_!=$entry, @$menu;
 		}
 	}
-	for my $eid (keys %FLmenuentry)
-	{	my $menu=\@FilterList::FLcMenu;
-		my $entry=$FLmenuentry{$eid};
-		if (!$removeall && $::Options{OPT.$eid})
-		{	push @$menu,$entry unless (grep $_==$entry, @$menu);
-		}
-		else
-		{	@$menu =grep $_!=$entry, @$menu;
-		}
-	}
 }
 
-sub checkvalidpath
-{	my $p= ::decode_url($::Options{OPT.'path'});
-	$p && (-d $p || $p=~m/[%\$]/);
-}
-sub Copy
+sub RunCommand_1
 {	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
-	my $path= ::decode_url( $::Options{OPT.'path'} );
-	::CopyMoveFiles($IDs,copy=>1, basedir=>$path,
-		dirformat	=> $::Options{OPT.'folderformat'},
-		filenameformat	=> $::Options{OPT.'filenameformat'},
-	);
-}
-
-sub ToM3U
-{	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
-	return unless @$IDs;
-	my $file=::ChooseSaveFile(undef,_"Write filenames to ...", Songs::Get($IDs->[0],'path'), 'list.m3u');
-	return unless defined $file;
-	my $content="#EXTM3U\n";
-	for my $ID (@$IDs)
-	{	my ($file,$length,$artist,$title)= Songs::Get($ID,qw/fullfilename length artist title/);
-		::_utf8_on($file); # for file, so it doesn't get converted in utf8
-		$content.= "\n#EXTINF:$length,$artist - $title\n$file\n";
-	}
-	{	my $fh;
-		open($fh,'>:utf8',$file) && (print $fh $content) && close($fh) && last;
-		my $res= ::Retry_Dialog($!,_"Error writing .m3u file", details=> ::__x( _"file: {filename}", filename=>$file));
-		redo if $res eq 'retry';
-	}
-}
-
-sub ToCSV
-{	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
-	my $check=::NewPrefCheckButton(OPT.'toCSV_notitlerow',_"Do not add a title row");
-	my $file=::ChooseSaveFile(undef,_"Write filenames to ...",undef,'songs.csv',$check);
-	return unless defined $file;
-	my @fields= (qw/file path/,sort grep !m/^file$|^path$/, (Songs::PropertyFields())); #make sure file and path are in position 0 and 1
-
-	my $retry;
-	{	if ($retry)
-		{	my $res= ::Retry_Dialog($!,_"Error writing .csv file", details=> ::__x( _"file: {filename}", filename=>$file));
-			last unless $res eq 'retry';
-		}
-		$retry=1;
-
-		open my$fh,'>:utf8',$file  or redo;
-		unless ($::Options{OPT.'toCSV_notitlerow'}) #print a title row
-		{	print $fh join(',',map Songs::FieldName($_), @fields)."\n"  or redo;
-		}
-		for my $ID (@$IDs)
-		{	my @val;
-			push @val, Songs::Get($ID,@fields);
-			s/\x00/\t/g for @val; #for genres and labels
-			s/"/""/g for @val;
-			::_utf8_on($val[0]); # for file and path, so it doesn't get converted in utf8
-			::_utf8_on($val[1]); # FIXME find a cleaner way to do that
-			#print STDERR join(',',@val)."\n";
-			print $fh join(',',map '"'.$_.'"', @val)."\n"  or redo;
-		}
-		close $fh  or redo;
-	}
-}
-
-sub RunCommand
-{	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
-	my $cmd= $::Options{OPT.'tocmd_cmd'};
+	my $cmd= $::Options{OPT.'tocmd_cmd_1'};
 	::run_system_cmd($cmd,$IDs,0);
 }
 
+sub RunCommand_2
+{	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
+	my $cmd= $::Options{OPT.'tocmd_cmd_2'};
+	::run_system_cmd($cmd,$IDs,0);
+}
+sub RunCommand_3
+{	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
+	my $cmd= $::Options{OPT.'tocmd_cmd_3'};
+	::run_system_cmd($cmd,$IDs,0);
+}
+
+sub RunCommand_4
+{	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
+	my $cmd= $::Options{OPT.'tocmd_cmd_4'};
+	::run_system_cmd($cmd,$IDs,0);
+}
+#sub RunCommand_5
+#{	my $IDs=$_[0]{IDs} || $_[0]{filter}->filter;
+#	my $cmd= $::Options{OPT.'tocmd_cmd_5'};
+#	::run_system_cmd($cmd,$IDs,0);
+#}
 
 1
